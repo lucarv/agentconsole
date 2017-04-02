666e5267dd37659f8761042dbe2594021b93c5f9
lucarv <lucarv@microsoft.com>
2017-04-02 20:06:48 +0200

    first version: able to invoke reboot and query

---
+++ app/controllers/home.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var express = require('express'),
+  router = express.Router();
+
+var Registry = require('azure-iothub').Registry;
+var Client = require('azure-iothub').Client;
+var deviceId = '';
+var client,
+  registry;
+
+
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+
+  res.render('index', {
+    title: 'utility mgmt console'
+  });
+});
+
+router.post('/', function (req, res, next) {
+
+  var connectionString = req.body.cs;
+  registry = Registry.fromConnectionString(connectionString);
+  client = Client.fromConnectionString(connectionString);
+  deviceId = req.body.devID;
+
+  res.render('commands', {
+    title: 'utility mgmt console',
+    deviceId: deviceId,
+    msg: 'ready to send command to device'
+  });
+});
+
+router.post('/dc', function (req, res, next) {
+  var msg = '';
+  switch (req.body.action) {
+    case 'reboot':
+      var methodName = "reboot";
+      var msg = '';
+      var methodParams = {
+        methodName: methodName,
+        payload: null,
+        timeoutInSeconds: 30
+      };
+
+      client.invokeDeviceMethod(deviceId, methodParams, function (err, result) {
+        console.log('requested reboot');
+
+        if (err) {
+          msg = "Direct method error: " + err.message;
+        } else {
+          msg = "Successfully invoked the device to reboot.";
+        }
+
+        res.render('commands', {
+          title: 'utility mgmt console',
+          deviceId: deviceId,
+          msg: msg
+        });
+      });
+      break;
+
+    case 'query':
+      registry.getTwin(deviceId, function (err, twin) {
+        console.log('started query on twin')
+        if (twin.properties.reported.iothubDM != null) {
+          if (err) {
+            msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
+          } else {
+            var lastRebootTime = twin.properties.reported.iothubDM.reboot.lastReboot;
+            msg = 'Last reboot time: ' + JSON.stringify(lastRebootTime, null, 2);
+          }
+        }
+        else
+          msg = 'no completed reboot has been reported by device';
+
+        res.render('commands', {
+          title: 'utility mgmt console',
+          deviceId: deviceId,
+          msg: msg
+        });
+
+      });
+
+      break;
+  }
+
+
+});
+
+
+
