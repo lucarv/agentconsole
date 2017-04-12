ab4fc999fd5dcfdc65477944305e6bd5f5ee300c
lucarv <lucarv@microsoft.com>
2017-04-12 20:27:22 +0200

    setting desired properties for reporting interval and firmware version

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -11,13 +11,106 @@ var connectionString = '';
 var client,
   registry;
 
-var location,
-  lastRebootTime,
+var location = 'not yet reported',
+  lastRebootTime = 'not yet reported',
   connType,
   version,
+  interval,
   msg;
 
-function displayTwinValues(err, twin) {
+function fetchReportedProperties(res, next) {
+  registry.getTwin(deviceId, function (err, twin) {
+    msg = 'reported properties';
+    if (twin.properties.reported.iothubDM != null) {
+      if (err) {
+        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
+      } else {
+        lastRebootTime = twin.properties.reported.iothubDM.reboot.lastReboot;
+      }
+    }
+    if (twin.properties.reported.location != null) {
+      if (err) {
+        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
+      } else {
+        location = twin.properties.reported.location.zipcode;
+      }
+    }
+    if (twin.properties.reported.connectivity != null) {
+      if (err) {
+        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
+      } else {
+        connType = twin.properties.reported.connectivity.type;
+      }
+    }
+    if (twin.properties.reported.fw_version != null) {
+      if (err) {
+        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
+      } else {
+        version = twin.properties.reported.fw_version.version;
+      }
+    }
+    if (twin.properties.reported.interval != null) {
+      if (err) {
+        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
+      } else {
+        interval = twin.properties.reported.interval.ms;
+      }
+    }
+    res.render('twin', {
+      title: 'utility mgmt console',
+      deviceId: deviceId,
+      location: location,
+      lastRebootTime: lastRebootTime,
+      connType: connType,
+      version: version,
+      interval: interval,
+      footer: 'reported properties'
+    });
+  });
+}
+
+function setDesiredProperties(res, next, choice, prop) {
+  registry.getTwin(deviceId, function (err, twin) {
+
+    if (err) {
+      console.error(err.constructor.name + ': ' + err.message);
+    } else {
+
+      switch (choice) {
+        case 'fw':
+          var patch = { properties: { desired: { fw: { version: prop } } } };
+          break;
+
+        case 'interval':
+          var patch = { properties: { desired: { interval: { ms: prop } } } };
+          break;
+      }
+      twin.update(patch, function (err) {
+        if (err) {
+          console.error('Could not update twin: ' + err.constructor.name + ': ' + err.message);
+        } else {
+          console.log(twin.deviceId + ' twin desired property updated successfully');
+          console.log();
+          console.log("Desired: " + JSON.stringify(twin.properties.desired));
+          console.log();
+
+          //queryTwins();
+
+        }
+      });
+    }
+
+    res.render('twindes', {
+      title: 'utility mgmt console',
+      deviceId: deviceId,
+      location: location,
+      lastRebootTime: lastRebootTime,
+      connType: connType,
+      version: version,
+      footer: 'desired properties saved'
+    });
+  });
+
 }
 
 module.exports = function (app) {
@@ -45,48 +138,43 @@ router.post('/', function (req, res, next) {
 
 router.get('/twin', function (req, res, next) {
   // fetch twin properties here
-  registry.getTwin(deviceId, function (err, twin) {
+  fetchReportedProperties(res, next);
+});
 
-    console.log('twin: ' + err);
-
-    if (twin.properties.reported.iothubDM != null) {
-      if (err) {
-        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
-      } else {
-        var lastRebootTime = twin.properties.reported.iothubDM.reboot.lastReboot;
-      }
-    }
-    if (twin.properties.reported.location != null) {
-      if (err) {
-        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
-      } else {
-        var location = twin.properties.reported.location.zipcode;
-      }
-    }
-    if (twin.properties.reported.connectivity != null) {
-      if (err) {
-        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
-      } else {
-        var connType = twin.properties.reported.connectivity.type;
-      }
-    }
-    if (twin.properties.reported.fw_version != null) {
-      if (err) {
-        msg = 'Could not query twins: ' + err.constructor.name + ': ' + err.message;
-      } else {
-        var version = twin.properties.reported.fw_version.version;
-      }
-    }
-
-    res.render('twin', {
-      title: 'utility mgmt console',
-      deviceId: deviceId,
-      location: location,
-      lastRebootTime: lastRebootTime,
-      connType: connType,
-      version: version
-    });
+router.get('/des', function (req, res, next) {
+  console.log(JSON.stringify(req.body));
+  res.render('twindes', {
+    title: 'utility mgmt console',
+    deviceId: deviceId,
+    version: version,
+    interval: interval,
+    footer: msg
   });
+});
+
+router.post('/des', function (req, res, next) {
+  setDesiredProperties(res, next, req.body.choice, req.body.prop);
+});
+
+router.post('/twin', function (req, res, next) {
+  switch (req.body.action) {
+    case 'refresh':
+      fetchReportedProperties(res, next);
+      break;
+    default:
+      msg = req.body.action;
+      res.render('twin', {
+        title: 'utility mgmt console',
+        deviceId: deviceId,
+        location: location,
+        lastRebootTime: lastRebootTime,
+        connType: connType,
+        version: version,
+        footer: msg
+      });
+      break;
+  }
+
 });
 
 router.get('/commands', function (req, res, next) {
